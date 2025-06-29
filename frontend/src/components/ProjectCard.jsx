2ec536cb1c1a6718529fa33e3a9996af1440a7c8
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-06-29 21:45:57 +1200

    style: update ProjectCard background to use a gradient overlay (to meet the design spec)

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -2,15 +2,21 @@ import InfoLabel from "./labels/InfoLabel";
 
 function ProjectCard({ project, className = "" }) {
   return (
-    <div
-      className={`${className} flex flex-col justify-end items-end w-full py-1 px-0.5 gap-1 h-20 bg-[url(/projectPhoto.png)] bg-cover`}
-    >
-      <h2 className="heading2 text-baseWhite line-clamp-2">
-        {project?.title ?? "Enhance human-food interaction in social settings "}
-      </h2>
-      <div className="self-start flex flex-row flex-wrap gap-0.5 w-full"><InfoLabel label="Understand"/><InfoLabel label="Augment"/><InfoLabel label="Assist"/></div>
-      
-    </div>
+    
+      <div
+        className={`${className} flex flex-col justify-end items-end w-full py-1 px-0.5 gap-1 h-20 bg-[linear-gradient(rgba(0,0,0,0.25),rgba(0,0,0,0.25)),url(/projectPhoto.png)] bg-cover`}
+      >
+        <h2 className="heading2 text-baseWhite line-clamp-2">
+          {project?.title ??
+            "Enhance human-food interaction in social settings "}
+        </h2>
+        <div className="self-start flex flex-row flex-wrap gap-0.5 w-full">
+          <InfoLabel label="Understand" />
+          <InfoLabel label="Augment" />
+          <InfoLabel label="Assist" />
+        </div>
+      </div>
+    
   );
 }
 

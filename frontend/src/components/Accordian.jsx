0e00911da3d33ca8492e1ea1981e4152b4594523
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-02 13:21:36 +1200

    feat: enhance Accordian component with SVG icons for expand/collapse functionality and improve styling

--- frontend/src/components/Accordian.jsx
+++ frontend/src/components/Accordian.jsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 import PublicationsContainer from "./PublicationsContainer";
+import down from "../assets/icons/down.svg";
+import up from "../assets/icons/up.svg";
 
 function Accordian({ data }) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <>
-      <div className="flex flex-col bg-cyan-700 w-full ">
-        <div className="flex flex-row w-full justify-between  py-0.5">
+      <div className="flex flex-col w-full gap-1.5 ">
+        <div className="flex flex-row w-full justify-between items-center py-0.5">
           <h2 className="heading4 ">{data.year}</h2>
           <button
-            className="label bg-background-primary text-white"
+            className="label text-white"
             onClick={() => {
               setIsExpanded((prev) => !prev);
             }}
           >
-            {isExpanded ? "hide" : "show"}
+            {/* {isExpanded ? "hide" : "show"} */}
+            <img src={isExpanded ? up : down}/>
           </button>
         </div>
         {isExpanded && <PublicationsContainer publications={data.items} />}

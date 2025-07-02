6381c9eb1a8cc2e291696f23208ec8605e5ed3b0
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-02 13:25:04 +1200

    fix: add margin-top to ProjectsPage container for improved layout

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -14,7 +14,7 @@ function ProjectsPage() {
   ];
   return (
     <>
-      <div className="w-screen bg-background-white">
+      <div className="mt-4 w-screen bg-background-white">
         <div className="border-2 border-amber-500 px-1.5 xl:px-0 flex flex-col justify-between items-start  py-5 gap-4 w-full xl:max-w-[1032px]  xl:mx-auto">
           <h1 className="heading1 self-start">Projects</h1>
           <button className="heading4 mt-1">Current Projects</button>

b3b249da13bd69632e70412e7766ff812d454fc7
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-02 13:18:48 +1200

    feat: add down and up SVG icons instead of buttons instead of text, add styling changes to publications page content

--- frontend/src/pages/PublicationsPage.jsx
+++ frontend/src/pages/PublicationsPage.jsx
@@ -3,25 +3,20 @@ import PublicationSectionWrapper from "../components/wrappers/PublicationSection
 import publications from "../data/publications";
 
 function PublicationsPage() {
-
-
   return (
     <>
       {/* <div className="w-screen bg-background-white">
-        <div className="border-2 border-amber-500 px-1.5 xl:px-0 flex flex-col justify-between items-start  py-5 gap-4 w-full xl:max-w-[1032px]  xl:mx-auto"> */}
-          {" "}
-          {/* <h1 className="heading1 self-start">Publications</h1> */}
-          <PublicationSectionWrapper headingContent="Publications">
-            {(
-              [...new Set (publications
-                .map((pub) => pub.year))]
-                .map((year) => {
-                  const items = publications.filter((pub) => pub.year === year);
-                  return <Accordian key={year} data={{ year, items }} />;
-                })
-            )}
-          </PublicationSectionWrapper>
-        {/* </div>
+        <div className="border-2 border-amber-500 px-1.5 xl:px-0 flex flex-col justify-between items-start  py-5 gap-4 w-full xl:max-w-[1032px]  xl:mx-auto"> */}{" "}
+      {/* <h1 className="heading1 self-start">Publications</h1> */}
+      <div className="mt-4">
+        <PublicationSectionWrapper headingContent="Publications">
+          {[...new Set(publications.map((pub) => pub.year))].map((year) => {
+            const items = publications.filter((pub) => pub.year === year);
+            return <Accordian key={year} data={{ year, items }} />;
+          })}
+        </PublicationSectionWrapper>
+      </div>
+      {/* </div>
       </div> */}
     </>
   );
